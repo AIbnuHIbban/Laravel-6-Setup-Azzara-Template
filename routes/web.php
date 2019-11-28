9cdc2deea5586d2438a49404c6dadc97637105fd
AibnuHibban <abd.ibnuhibban@gmail.com>
2019-11-28 08:58:48 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
